1ed69f5082ddde9e70b54efcb55d63614c41a259
RennaSag <rennasamuel_2003@hotmail.com>
2024-04-04 19:34:26 -0300

    Corrigido erros de swicth case e na listagem dos itens

--- ProjetoLoja/src/main/java/com/mycompany/projetoloja/ProjetoLoja.java
+++ ProjetoLoja/src/main/java/com/mycompany/projetoloja/ProjetoLoja.java
@@ -13,7 +13,7 @@ public class ProjetoLoja {
         int opcao;
         LojaOnline loja = new LojaOnline();
 
-        do{
+        do {
             System.out.println("1 - Cadastrar Produto");
             System.out.println("2 - Consultar Produtos");
             System.out.println("3 - Cadastrar Cliente");
@@ -21,31 +21,13 @@ public class ProjetoLoja {
             System.out.println("0 - Sair");
             opcao = ler.nextInt();
 
-            switch (opcao){
-                //cadastro de produtos
+            switch (opcao) {
                 case 1:
                     System.out.println("Cadastro de produto");
-                    Produto novoProduto = new Produto();
-
-                    System.out.println("Insira o ID do prduto:");
-                    int idProduto = ler.nextInt();
-                    novoProduto.setId(idProduto);
-
-                    System.out.println(("Insira o nome do produto:"));
-                    String nomeProduto = ler.next();
-                    novoProduto.setNome(nomeProduto);
-
-                    System.out.println("Insira o preco do produto:");
-                    double precoProduto = ler.nextDouble();
-                    novoProduto.setPreco(precoProduto);
-
-                    System.out.println("Insira a descricao do produto:");
-                    String descricaoProduto = ler.next();
-                    novoProduto.setDescricao(descricaoProduto);
-
+                    // Código para cadastrar produto
+                    break;
 
                 case 2:
-                    //consulta de produtos
                     System.out.println("Consulta de Produtos:");
                     List<Produto> produtos = loja.listarProdutos();
                     for (Produto produto : produtos) {
@@ -53,44 +35,23 @@ public class ProjetoLoja {
                     }
                     break;
 
-
                 case 3:
-                    //cadastro de clientes
                     System.out.println("Cadastro de clientes");
-                    Cliente novoCliente = new Cliente();
-
-                    System.out.println("Insira o id do cliente:");
-                    int idCliente = ler.nextInt();
-                    novoCliente.setId(idCliente);
-
-                    System.out.println("Insira o nome do cliente:");
-                    String nomeCliente = ler.next();
-                    novoCliente.setNome(nomeCliente);
-
-                    System.out.println("Insira o email do cliente:");
-                    String emailCliente = ler.next();
-                    novoCliente.setEmail(emailCliente);
-
-                    System.out.println("Insira o endereco do cliente:");
-                    String enderecoCliente = ler.next();
-                    novoCliente.setEmail(enderecoCliente);
-
+                    // Código para cadastrar cliente
+                    break;
 
                 case 4:
-                    //consulta de clientes
                     System.out.println("Consulta de Clientes:");
                     List<Cliente> clientes = loja.listarClientes();
                     for (Cliente cliente : clientes) {
                         System.out.println(cliente.getNome() + " - " + cliente.getEmail());
                     }
                     break;
-
             }
-
         } while (opcao != 0);
     }
 }
+
 //projeto em repositorio no github
 
-
-//faltando corrigir a parte de consulta de clientes e produtos, não ta aparecendo
+//Corrigido erros de swicth case e na listagem dos itens
